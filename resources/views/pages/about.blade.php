bf719474d3b7748f4593fbfee03a4f2305874362
TSMMONKEY <thabanimat24@gmail.com>
2023-07-13 14:21:39 +0200

    added all files after creating a view for all the pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,319 @@
+@extends('layouts.main')
+
+@section('title', 'About')
+
+@section('content')
+
+    <section class="news-detail-header-section text-center">
+        <div class="section-overlay"></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-12">
+                    <h1 class="text-white">
+                        About GSI Security Services
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="news-section section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-12">
+                    <div class="news-block">
+                        <div class="news-block-top">
+                            <h2>Overview</h2>
+                            <img src="images/about-overview.webp" class="news-image img-fluid" alt=""
+                                width="100%" />
+
+                        </div>
+                    </div>
+                    <div class="news-block-info">
+                        <div class="news-block-title mb-2">
+                        </div>
+
+                        <div class="news-block-body">
+                            <h2>GSI Security Services</h2>
+                            <p>
+                                <strong>GSI
+                                    Security Services</strong>, we are a dedicated team of
+                                professionals committed to providing top-notch security
+                                solutions for our clients. With years of experience and a
+                                deep understanding of the security industry, we offer a
+                                comprehensive range of services designed to meet the
+                                unique needs of businesses and individuals. Our team
+                                consists of highly trained security personnel who possess
+                                the expertise and skills necessary to ensure the safety
+                                and protection of our clients. We take pride in our
+                                commitment to excellence and strive to deliver exceptional
+                                security services that exceed expectations.
+                            </p>
+
+                            <p>
+                                <strong>A Proudly South African BBBEE owned
+                                    company</strong>. Established in 2009, GSI
+                                security is a reputable, security training academy. We have
+                                a proven clean track record which we have acquired over
+                                14 years in the industry. Our over a million graduates are
+                                PSIRA accredited.
+                                The Directors combined security experience of 55 years
+                                coupled with our PSIRA accredited and SASSETA qualified
+                                assessors and moderators we ensure that
+                                our instructors are qualified to provide the required
+                                standard of specialised training in the following
+                                areas of security:
+                            <ul>
+                                <li>Grade E to A</li>
+                                <li>Cash In Transit(CIT)</li>
+                                <li> Satellite tracking</li>
+                                <li> Forensics</li>
+                                <li> Private investigation</li>
+                            </ul>
+                            </p>
+
+                            <blockquote>
+                                The mission of GSI Security Services is to deliver quality training,
+                                provide a highly skilled workforce, and offer value-added security services to the
+                                public and private sector
+                            </blockquote>
+                        </div>
+
+                        <div class="row mt-5 mb-4" style="margin: 0 auto; width: 98%;">
+                            <div class="col-lg-6 col-12 mb-4 mb-lg-0">
+                                <img src="images/AdobeStock_542287786_Preview.webp" class="news-detail-image img-fluid"
+                                    alt="" />
+                            </div>
+
+                            <div class="col-lg-6 col-12">
+                                <img src="images/AdobeStock_570876060_Preview.webp" class="news-detail-image img-fluid"
+                                    alt="" />
+                            </div>
+                        </div>
+
+                        <h2>Our Mission</h2>
+                        <p>
+                            At GSI Security Services, our mission is to provide unparalleled security solutions to our
+                            clients, ensuring their safety and peace of mind. We are driven by a commitment to excellence,
+                            professionalism, and integrity in all aspects of our work.<br><br>
+
+                            We understand that security is a fundamental need for businesses and individuals alike. Our
+                            mission is to create a safe and secure environment that allows our clients to focus on their
+                            core activities without worrying about potential threats or vulnerabilities.<br><br>
+
+                            To achieve this mission, we employ a highly trained team of security professionals who possess
+                            the expertise and skills necessary to handle diverse security challenges. We continuously invest
+                            in their training and development, ensuring they stay abreast of the latest industry trends,
+                            technological advancements, and best practices in security management.<br><br>
+
+                            Through a collaborative approach, we work closely with our clients to understand their specific
+                            security requirements, concerns, and goals. This enables us to tailor our services and solutions
+                            to meet their unique needs effectively. Whether it's implementing state-of-the-art surveillance
+                            systems, conducting comprehensive security assessments, or developing emergency response plans,
+                            we strive to deliver customized strategies that mitigate risks and enhance security.
+                        </p><br><br>
+
+                        <h2>Our Vision</h2>
+                        <p>
+                            At GSI Security Services, we embrace a set of core values that guide our operations and shape
+                            our relationships with clients, employees, and the community. In addition to the existing
+                            values, we are committed to the
+                            following seven values:
+                        <section class="our-vision-inner-section">
+                            <ul>
+                                <li>Client solutions</li>
+                                <li>Sustainable relationships</li>
+                                <li>Transparency</li>
+                                <li>Reliability</li>
+                                <li>Commitment to quality</li>
+                                <li>Training</li>
+                                <li>Ubuntu</li>
+                            </ul>
+                            <ul>
+                                <li>Cultural diversity</li>
+                                <li>Innovation and constancy</li>
+                                <li>Collaborative work site</li>
+                                <li>Continuous Improvement</li>
+                                <li>Ethics and Integrity</li>
+                                <li>Proactive Approach</li>
+                                <li>Social Responsibility</li>
+                            </ul>
+                        </section>
+                        </p>
+
+                        <div class="social-share border-top mt-5 py-4 d-flex flex-wrap align-items-center">
+                        </div>
+                        <form class="custom-form comment-form mt-4" action="" method="post" role="form"
+                            style="
+                    margin: 0 auto;
+                    text-align: center;
+                ">
+                            <h6 class="mb-3">Write a comment</h6>
+
+                            <textarea name="comment-message" rows="4" class="form-control" id="comment-message"
+                                placeholder="Your comment here"></textarea>
+
+                            <div class="col-lg-3 col-md-4 col-6 ms-auto" style="margin: 0 auto;">
+                                <button type="submit" class="form-control">
+                                    Comment
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-12 mx-auto mt-4 mt-lg-0" id="More-Section-Info" style="width: 41%;">
+                    <h5 class="mt-5 mb-3">More</h5>
+
+                    <section class="more-info">
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/AdobeStock_443291657_Preview.jpeg" class="news-image img-fluid"
+                                        alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#" class="news-block-title-link">Grade E - A Training</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get E - A Grade Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/ecd-training.jpg" class="news-image img-fluid" alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#" class="news-block-title-link">EDC Training</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get your EDC Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/training-course.jpg" class="news-image img-fluid" alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#" class="news-block-title-link">CIT</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get Your Cash In Transit Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/training.jpg" class="news-image img-fluid" alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#" class="news-block-title-link">SE Training</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get Special Event<br> Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/pi-trainng.jpg" class="news-image img-fluid" alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#" class="news-block-title-link">PI Training</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get Your PI Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="width: 48%;" class="news-block news-block-two-col d-flex mt-4">
+                            <div class="news-block-two-col-image-wrap-about more-info__section">
+                                <a href="/#section_3">
+                                    <img src="images/vision.jpg" class="news-image img-fluid" alt="" />
+                                </a>
+                            </div>
+
+                            <div class="news-block-two-col-info">
+                                <div class="news-block-title mb-2">
+                                    <h6>
+                                        <a href="/#section_3" class="news-block-title-link">Grade B Training</a>
+                                    </h6>
+                                </div>
+
+                                <div class="news-block-date">
+                                    <p>
+                                        Get your Grade B Training
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <form class="custom-form subscribe-form" action="" method="post" role="form">
+                        <h5 class="mb-4">Newsletter Form</h5>
+
+                        <input type="email" name="subscribe-email" id="subscribe-email" pattern="[^ @]*@[^ @]*"
+                            class="form-control" placeholder="Email Address" required />
+
+                    </form>
+                    <div class="col-lg-12 col-12">
+                        <button type="submit" class="form-control">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+@endsection
