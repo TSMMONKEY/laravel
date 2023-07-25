71deef8af86c7701e93e581060b4a61fc1edcd4f
TSMMONKEY <thabanimat24@gmail.com>
2023-07-25 15:58:08 +0200

    made seeders for users table and for role table. Made one-to-many relationship between user  and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
 {
     return $this->is_admin;
 }
+
+public function role(): BelongsTo
+{
+    return $this->belongsTo(Role::class);
+}
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,29 +5,39 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
-class RoleSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('roles')->insert([
-            'title' => 'admin',
+        DB::table('users')->insert([
+            'name' =>'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('pass12345'),
+            'role_id' => 1,
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
-        
-        DB::table('roles')->insert([
-            'title' => 'agent',
+
+        DB::table('users')->insert([
+            'name' =>'agent',
+            'email' => 'agent@gmail.com',
+            'password' => Hash::make('pass12345'),
+            'role_id' => 2,
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
-        
-        DB::table('roles')->insert([
-            'title' => 'user',
+
+        DB::table('users')->insert([
+            'name' =>'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('pass12345'),
+            'role_id' => 3,
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
